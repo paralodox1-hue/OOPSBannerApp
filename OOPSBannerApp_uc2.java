0aa9ad481c0c223f2a3502696a5eabc3589a9ee0
paralodox1-hue <paralodox1@gmail.com>
2026-02-16 14:27:28 +0530

    Add files via upload

---
+++ OOPSBannerApp_uc2.java
@@ -0,0 +1,12 @@
+public class OOPSBannerApp_uc2 {
+
+    public static void main(String[] args) {
+        System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "+"    *****");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "+" ***     ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"***      ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**   **  "+" "+"   ***   ");
+        System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "+"      ***");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+"     *** ");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "+"*****    ");
+    }
+}
