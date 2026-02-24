6fb3f27db1eb659a3de5607abed499b7f0863f40
paralodox1-hue <paralodox1@gmail.com>
2026-02-24 10:16:22 +0530

    UC1 implemented

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,6 @@
-public class OOPSBannerApp {
+public class OOPSBannerApp{
+
     public static void main(String[] args) {
-        System.out.println("OOPS Banner Application");
+        System.out.println("OOPS");
     }
 }
